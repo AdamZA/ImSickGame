4b102e3ee9cf0478a0aab76be872fa5517f8dd85
Adam Sundstrom <adamrayner@live.com>
2015-05-08 12:11:48 +0200

    Added player movement and basic collision in the level1 scene.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float speed = 4.02f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (Input.touchCount > 0) {
+			// The screen has been touched so store the touch
+			Touch touch = Input.GetTouch(0);
+			
+			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+				// If the finger is on the screen, move the object smoothly to the touch position
+				Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));                
+				transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/cellDestroyScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cellDestroyScript : MonoBehaviour 
+{
+	public GameObject cellObject;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	//basic collision script
+	void OnCollisionEnter(Collision c)
+	{
+		if(c.gameObject.tag.Equals("Player"))
+			Destroy (gameObject);
+	}
+
+
+}
